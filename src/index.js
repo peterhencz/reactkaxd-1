a3df9ace2e8c9ddae55ab0c84fe53532dd77a6aa
Balázs Baumgartner <balazskaxd@gmail.com>
2018-06-27 21:18:49 +0200

    Added hot reload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import Root from './components/Root';
 
 
 render(
   <AppContainer>
-    <h1>Hello World!!</h1>
+    <Root />
   </AppContainer>,
   document.getElementById('app'),
 );
+
+if (module.hot) {
+  module.hot.accept('./components/Root', () => {
+    const NewRoot = require('./components/Root').default; // eslint-disable-line
+    render(
+      <AppContainer>
+        <NewRoot />
+      </AppContainer>,
+      document.getElementById('app'),
+    );
+  });
+}
