e4525f56b3a97abfb8a6930b401e4897c951bb03
Balázs Baumgartner <balazskaxd@gmail.com>
2018-06-27 20:48:52 +0200

    Initial state

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+
+render(
+  <AppContainer>
+    <h1>Hello World!!</h1>
+  </AppContainer>,
+  document.getElementById('app'),
+);
---
+++ webpack.config.dev.js
@@ -0,0 +1,89 @@
+import webpack from 'webpack';
+import HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import path from 'path';
+import autoprefixer from 'autoprefixer';
+
+const inProject = path.resolve.bind(path, __dirname);
+
+
+export default {
+  resolve: {
+    modules: [
+      inProject('src'),
+      'node_modules',
+    ],
+    extensions: ['*', '.js', '.jsx', '.json'],
+  },
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    './tools/webpack-public-path',
+    'react-hot-loader/patch',
+    'webpack-hot-middleware/client?reload=true',
+    path.resolve(__dirname, 'src/index.js'),
+  ],
+  target: 'web',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HardSourceWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+      inject: true,
+    }),
+    new ExtractTextPlugin({
+      filename: 'styles/[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugin: () => [
+                autoprefixer,
+              ],
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: [
+                path.resolve('src'),
+                path.resolve('src', 'styles'),
+              ],
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
